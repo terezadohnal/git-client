c52f1396b90c4171d95f83f1271a1ed578f040a9
Tereza Dohnalova <tereza.dohnalova@notum.cz>
2023-02-05 20:18:38 +0100

    new: add commit graph to repository page

--- src/pages/Repository.tsx
+++ src/pages/Repository.tsx
@@ -5,7 +5,23 @@ import {
   useAppStateDispatch,
 } from 'context/AppStateContext/AppStateProvider';
 import { Directory } from 'helpers/types';
-import { useCallback, useEffect } from 'react';
+import { useCallback, useEffect, useMemo } from 'react';
+import { MultiGraph } from 'graphology';
+import { SigmaContainer, useLoadGraph } from '@react-sigma/core';
+import '@react-sigma/core/lib/react-sigma.min.css';
+
+const LoadGraph = (props: { data: any }) => {
+  const { data } = props;
+  const loadGraph = useLoadGraph();
+
+  useEffect(() => {
+    const graph = new MultiGraph();
+    graph.import(data);
+    loadGraph(graph);
+  }, [loadGraph, data]);
+
+  return null;
+};
 
 export const Repository = () => {
   const appState = useAppState();
@@ -22,18 +38,58 @@ export const Repository = () => {
         commits: parsedDir.commits,
       },
     });
-  }, [appState, appStateDispatch]);
-
-  console.log(appState.commits);
+  }, [appState.repositoryPath, appStateDispatch]);
 
   useEffect(() => {
     fetchDirectory();
   }, [fetchDirectory]);
 
+  const { commits } = appState;
+
+  const data = useMemo(() => {
+    return {
+      nodes: commits.reverse().map((commit, index) => ({
+        key: commit.hash,
+        attributes: {
+          label: commit.message,
+          x: 0,
+          y: index,
+          size: 10,
+        },
+      })),
+      edges: commits
+        .reverse()
+        .slice(0, commits.length - 1)
+        .map((commit, index) => ({
+          key: commit.hash,
+          source: commit.hash,
+          target: commits[index + 1]?.hash,
+          attributes: {
+            size: 5,
+          },
+        })),
+    };
+  }, [commits]);
+
   return (
-    <Grid>
-      <Text h3>Repository</Text>
-      <Text>{appState.repositoryPath}</Text>
-    </Grid>
+    <Grid.Container css={{ h: '100vh', w: '1024px' }} justify="center">
+      <Text h4>Repository page</Text>
+      <SigmaContainer
+        style={{
+          height: '3000px',
+          width: '100%',
+          overflow: 'scroll',
+          padding: 0,
+        }}
+        settings={{
+          maxCameraRatio: 1,
+          minCameraRatio: 1,
+          defaultNodeColor: '#BC9EC1',
+          defaultEdgeColor: '#BC9EC1',
+        }}
+      >
+        <LoadGraph data={data} />
+      </SigmaContainer>
+    </Grid.Container>
   );
 };
